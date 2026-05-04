b9173ed70c4ce02924ac1635e6ed077128797d20
Matteo Dell'Acqua <matteodellacqua@ibilab.it>
2026-05-04 09:27:52 +0200

    Rename library and add installer translations

--- Manager.Vanguard.Common/Class1.cs
+++ Manager.Vanguard.Common/Class1.cs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace Manager.Vanguard.Lib
+namespace Manager.Vanguard.Common
 {
     public class Class1 { }
 }
