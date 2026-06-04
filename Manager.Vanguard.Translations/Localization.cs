95140b2192eb9a0abb5f50c9497f69b8242bd9ce
Matteo Dell'Acqua <matteodellacqua@ibilab.it>
2026-06-04 09:02:37 +0200

    Update license header for source files

--- Manager.Vanguard.Translations/Localization.cs
+++ Manager.Vanguard.Translations/Localization.cs
@@ -1,18 +1,19 @@
 // Copyright (C) 2026 Matteo Dell'Acqua
 //
-// This program is free software: you can redistribute it and/or modify
+// This file is part of Vanguard Manager.
+//
+// Vanguard Manager is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// Vanguard Manager is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with Vanguard Manager. If not, see <http://www.gnu.org/licenses/>.
 
 using Manager.Vanguard.Translations.Resources;
 
