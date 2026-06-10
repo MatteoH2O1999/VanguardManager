8c444b6fd32da508b93a146dd49ee765134b6465
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2026-06-10 20:49:38 +0200

    Implement logging for VanguardManager class

--- Manager.Vanguard.Service/VanguardManager.cs
+++ Manager.Vanguard.Service/VanguardManager.cs
@@ -20,7 +20,7 @@ using static Vanara.PInvoke.AdvApi32;
 
 namespace Manager.Vanguard.Service
 {
-    internal sealed class VanguardManager(ILogger<VanguardManager> Logger, ServiceManager SCM)
+    internal sealed partial class VanguardManager(ILogger<VanguardManager> Logger, ServiceManager SCM)
     {
         private const int SHUTDOWN_CHECK_INTERVAL_SECONDS =
 #if DEBUG
@@ -40,100 +40,155 @@ namespace Manager.Vanguard.Service
 
         public void ActivateVanguard()
         {
-            this.logger.LogInformation("Activating Vanguard services");
-
-            this.serviceManager.SetStart(ApplicationData.KernelLevelServiceName, ServiceStartType.SERVICE_SYSTEM_START);
-            this.logger.LogInformation(
-                $"Kernel level driver {{}} start mode set to {nameof(ServiceStartType.SERVICE_SYSTEM_START)}",
-                ApplicationData.KernelLevelServiceName
-            );
+            this.LogActivatingVanguard();
 
             this.serviceManager.SetStart(ApplicationData.UserLevelServiceName, ServiceStartType.SERVICE_DEMAND_START);
-            this.logger.LogInformation(
-                $"User level service {{}} start mode set to {nameof(ServiceStartType.SERVICE_DEMAND_START)}",
-                ApplicationData.UserLevelServiceName
-            );
+            this.LogActivatedUserService(ApplicationData.UserLevelServiceName);
 
-            this.logger.LogInformation("Vanguard services activated");
+            this.serviceManager.SetStart(ApplicationData.KernelLevelServiceName, ServiceStartType.SERVICE_SYSTEM_START);
+            this.LogActivatedKernelDriver(ApplicationData.KernelLevelServiceName);
+
+            this.LogActivatedVanguard();
         }
 
         public void DeactivateVanguard()
         {
-            this.logger.LogInformation("Deactivating Vanguard services");
+            this.LogDeactivatingVanguard();
 
             this.serviceManager.SetStart(ApplicationData.UserLevelServiceName, ServiceStartType.SERVICE_DISABLED);
-            this.logger.LogInformation(
-                $"User level service {{}} start mode set to {nameof(ServiceStartType.SERVICE_DISABLED)}",
-                ApplicationData.UserLevelServiceName
-            );
+            this.LogDeactivatedUserService(ApplicationData.UserLevelServiceName);
 
             this.serviceManager.SetStart(ApplicationData.KernelLevelServiceName, ServiceStartType.SERVICE_DISABLED);
-            this.logger.LogInformation(
-                $"Kernel level driver {{}} start mode set to {nameof(ServiceStartType.SERVICE_DISABLED)}",
-                ApplicationData.KernelLevelServiceName
-            );
+            this.LogDeactivatedKernelDriver(ApplicationData.KernelLevelServiceName);
 
-            this.logger.LogInformation("Vanguard services deactivated");
+            this.LogDeactivatedVanguard();
         }
 
         public async Task ShutdownVanguard(CancellationToken stoppingToken)
         {
-            this.logger.LogInformation("Shutting down Vanguard services");
+            this.LogShuttingDownVanguard();
 
             if (UserLevelServiceState != ServiceState.SERVICE_STOPPED)
             {
                 this.serviceManager.Stop(ApplicationData.UserLevelServiceName);
-                this.logger.LogInformation(
-                    "Requested immediate shutdown of user level service {}",
-                    ApplicationData.UserLevelServiceName
-                );
+                this.LogUserServiceShutdown(ApplicationData.UserLevelServiceName);
             }
 
             if (KernelDriverState != ServiceState.SERVICE_STOPPED)
             {
                 this.serviceManager.Stop(ApplicationData.KernelLevelServiceName);
-                this.logger.LogInformation(
-                    "Requested immediate shutdown of kernel level driver {}",
-                    ApplicationData.KernelLevelServiceName
-                );
+                this.LogKernelDriverShutdown(ApplicationData.KernelLevelServiceName);
             }
 
+            this.LogWaitingForVanguardShutdown();
             await this.WaitForVanguardShutdown(stoppingToken);
 
-            this.logger.LogInformation("Vanguard services successfully shut down");
+            this.LogVanguardShutdown();
         }
 
         private async Task WaitForVanguardShutdown(CancellationToken stoppingToken)
         {
+            string driverName = ApplicationData.KernelLevelServiceName;
+            string serviceName = ApplicationData.UserLevelServiceName;
             ServiceState kernelDriverState = KernelDriverState;
             ServiceState userServiceState = UserLevelServiceState;
 
-            this.logger.LogInformation(
-                "Kernel level driver {} state: {}; User level service {} state: {}",
-                ApplicationData.KernelLevelServiceName,
-                kernelDriverState,
-                ApplicationData.UserLevelServiceName,
-                userServiceState
-            );
+            this.LogVanguardState(driverName, kernelDriverState, serviceName, userServiceState);
 
             while (
                 kernelDriverState != ServiceState.SERVICE_STOPPED || userServiceState != ServiceState.SERVICE_STOPPED
             )
             {
-                this.logger.LogInformation($"Waiting {SHUTDOWN_CHECK_INTERVAL_SECONDS} seconds");
+                this.LogWaitForVanguardShutdownDelay(SHUTDOWN_CHECK_INTERVAL_SECONDS);
+
                 await Task.Delay(SHUTDOWN_CHECK_INTERVAL, stoppingToken);
 
                 kernelDriverState = KernelDriverState;
                 userServiceState = UserLevelServiceState;
 
-                this.logger.LogInformation(
-                    "Kernel level driver {} state: {}; User level service {} state: {}",
-                    ApplicationData.KernelLevelServiceName,
-                    kernelDriverState,
-                    ApplicationData.UserLevelServiceName,
-                    userServiceState
-                );
+                this.LogVanguardState(driverName, kernelDriverState, serviceName, userServiceState);
             }
         }
+
+        #region ActivateVanguardLogging
+
+        [LoggerMessage(LogLevel.Information, "Activating Vanguard services")]
+        private partial void LogActivatingVanguard();
+
+        [LoggerMessage(
+            LogLevel.Information,
+            $"User level service {{serviceName}} start mode set to {nameof(ServiceStartType.SERVICE_DEMAND_START)}"
+        )]
+        private partial void LogActivatedUserService(string serviceName);
+
+        [LoggerMessage(
+            LogLevel.Information,
+            $"Kernel level driver {{driverName}} start mode set to {nameof(ServiceStartType.SERVICE_SYSTEM_START)}"
+        )]
+        private partial void LogActivatedKernelDriver(string driverName);
+
+        [LoggerMessage(LogLevel.Information, "Vanguard services activated")]
+        private partial void LogActivatedVanguard();
+
+        #endregion
+
+        #region DeactivateVanguard Logging
+
+        [LoggerMessage(LogLevel.Information, "Deactivating Vanguard services")]
+        private partial void LogDeactivatingVanguard();
+
+        [LoggerMessage(
+            LogLevel.Information,
+            $"User level service {{serviceName}} start mode set to {nameof(ServiceStartType.SERVICE_DISABLED)}"
+        )]
+        private partial void LogDeactivatedUserService(string serviceName);
+
+        [LoggerMessage(
+            LogLevel.Information,
+            $"Kernel level driver {{driverName}} start mode set to {nameof(ServiceStartType.SERVICE_DISABLED)}"
+        )]
+        private partial void LogDeactivatedKernelDriver(string driverName);
+
+        [LoggerMessage(LogLevel.Information, "Vanguard services deactivated")]
+        private partial void LogDeactivatedVanguard();
+
+        #endregion
+
+        #region ShutdownVanguard Logging
+
+        [LoggerMessage(LogLevel.Information, "Shutting down Vanguard services")]
+        private partial void LogShuttingDownVanguard();
+
+        [LoggerMessage(LogLevel.Information, "Requested immediate shutdown of user level service {serviceName}")]
+        private partial void LogUserServiceShutdown(string serviceName);
+
+        [LoggerMessage(LogLevel.Information, "Requested immediate shutdown of kernel level driver {driverName}")]
+        private partial void LogKernelDriverShutdown(string driverName);
+
+        [LoggerMessage(LogLevel.Information, "Waiting for Vanguard services shutdown")]
+        private partial void LogWaitingForVanguardShutdown();
+
+        [LoggerMessage(LogLevel.Information, "Vanguard services successfully shut down")]
+        private partial void LogVanguardShutdown();
+
+        #endregion
+
+        #region WaitForVanguardShutdown Logging
+
+        [LoggerMessage(
+            LogLevel.Debug,
+            "Kernel level driver {driverName} state: {driverState}; User level service {serviceName} state: {serviceState}"
+        )]
+        private partial void LogVanguardState(
+            string driverName,
+            ServiceState driverState,
+            string serviceName,
+            ServiceState serviceState
+        );
+
+        [LoggerMessage(LogLevel.Trace, "Waiting {seconds} seconds")]
+        private partial void LogWaitForVanguardShutdownDelay(int seconds);
+
+        #endregion
     }
 }
